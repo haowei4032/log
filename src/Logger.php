5e97d4759cb1a5ee89de1030d4297dba1f0faa02
haowei <haow@lrwanche.com>
2018-07-17 11:22:42 +0800

    Add a custom log format

--- src/Logger.php
+++ src/Logger.php
@@ -26,7 +26,57 @@ class Logger
     {
         if (empty(static::$settings))
             throw new \ErrorException('EastWood Log loading configuration error');
+
+        if (!isset(static::$settings['format']))
+            static::$settings['format'] = '{date_rfc}||{timestamp}||{host}||{uri}||{verb}||{app}||{level}||{tag}||{message}';
         return static::$settings;
+    }
+
+    /**
+     * Logger format varable
+     * @return array
+     */
+    public static function getVariable()
+    {
+        $variable = [
+            'date' => function ($format = 'r') {
+                return date($format);
+            },
+            'date_rfc2822' => date('r'),
+            'timestamp' => microtime(true),
+            'unixtime' => time(),
+            'pid' => getmypid(),
+            'gid' => getmygid(),
+            'current_user' => get_current_user(),
+            'message' => '{message}',
+            'tag' => '{tag}',
+            'level' => '{level}',
+            'application' => '{application}',
+            'request_host' => isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '-',
+            'request_uri' => isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '-',
+            'request_method' => isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : '-',
+            'request_body' => file_get_contents('php://input'),
+            'request_addr' => isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '-',
+            'request_port' => isset($_SERVER['REMOTE_PORT']) ? $_SERVER['REMOTE_PORT'] : '-',
+            'request_user_agent' => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '-',
+            'server_addr' => isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '-',
+            'server_port' => isset($_SERVER['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : '-',
+            'server_protocol' => isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : '-',
+            'scheme' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') ? 'https' : 'http'
+        ];
+
+        /**
+         * variable alias
+         */
+        $variable['date_rfc'] = &$variable['date_rfc2822'];
+        $variable['app'] = &$variable['application'];
+        $variable['host'] = &$variable['request_host'];
+        $variable['uri'] = &$variable['request_uri'];
+        $variable['verb'] = &$variable['request_method'];
+        $variable['body'] = &$variable['request_body'];
+        $variable['user_agent'] = &$variable['request_user_agent'];
+
+        return $variable;
     }
 
     /**
@@ -66,19 +116,36 @@ class Logger
         }
 
         $output = [];
-        $output['message'] = implode('||', [
-            date('r'),
-            microtime(true),
-            isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '-',
-            isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '-',
-            isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : '-',
-            $settings['application'],
-            $level,
-            $tag,
-            $message
-        ]);
-
+        $output['message'] = static::$settings['format'];
         $output['path'] = implode(DIRECTORY_SEPARATOR, [$path, $filename]);
+        $varable = static::getVariable();
+        foreach ($varable as $name => $value) {
+            switch ($name) {
+                case 'tag':
+                    $value = $tag;
+                    break;
+                case 'level':
+                    $value = $level;
+                    break;
+                case 'app':
+                case 'application':
+                    $value = static::$settings['application'];
+                    break;
+                case 'message':
+                    $value = $message;
+                    break;
+                case 'date':
+                    $hit = preg_match('#{(<date>\((.+)\))}#', $output['message'], $match);
+                    if ($hit) {
+                        list(, $name, $format) = $match;
+                        $value = $value($format);
+                    } else {
+                        continue 2;
+                    }
+                    break;
+            }
+            $output['message'] = str_replace('{' . $name . '}', $value, $output['message']);
+        }
         return file_put_contents($output['path'], $output['message'] . PHP_EOL, FILE_APPEND);
     }
 
