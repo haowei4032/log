a86b9c6cf07368540abbbb1d85cbfc3886368d07
haowei <haow@lrwanche.com>
2018-07-17 11:23:11 +0800

    Add a custom log format

--- test.php
+++ test.php
@@ -7,6 +7,9 @@ use EastWood\Log\Logger;
 Logger::set([
     'path' => '/data1/logs',
     'rotate' => 'daily',
-    'application' => 'web'
+    'application' => 'web',
+    'format' => '{date_rfc}||{timestamp}||{application}||{verb}||{host}||{uri}||{level}||{tag}||{message}'
 ]);
+
+//var_dump( Logger::getVariable() );
 Logger::info('我是标签', '我是内容');
